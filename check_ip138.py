f80d830925e0a2e91c45eeb337cfd8ccc86389fc
thefuyang <thefuyang@gmail.com>
2016-02-27 17:06:46 +0800

    修复中文乱码 修复使用域名得不到IP的问题

--- check_ip138.py
+++ check_ip138.py
@@ -1,45 +1,50 @@
-#!/usr/bin/python
-#coding:utf-8
+#!/usr/bin/env python
+# coding:utf-8
 import urllib
 import re
 import sys
-
+import types
 
 
 def ISIP(s):
-    return len([i for i in s.split('.') if (0<= int(i)<= 255)])== 4
+    return len([i for i in s.split('.') if (0 <= int(i) <= 255)]) == 4
+
 
 def URL(ip):
-	uip=urllib.urlopen('http://wap.ip138.com/ip.asp?ip=%s'%ip)
-	fip=uip.read()
-	rip=re.compile(r"<br/><b>查询结果：(.*)</b><br/>")
-	result=rip.findall(fip)
-	print "%s\t %s" %(ip,result[0])
+    uip = urllib.urlopen('http://wap.ip138.com/ip.asp?ip=%s' % ip)
+    fip = uip.read()
+    rip = re.compile(r"<br/><b>查询结果：(.*)</b><br/>")
+    result = rip.findall(fip)
+    # print result[0]
+    # print result[0].encode('gbk')
+    # print result[0].decode('utf-8')
+    print u"%s\t %s" % (ip, result[0].decode('utf-8'))
+
 
 def DO(domain):
-        url=urllib.urlopen('http://wap.ip138.com/ip.asp?ip=%s'%domain)
-        f=url.read()
-        r=re.compile(r'&gt; (.*)<br/><b>查询结果：(.*)</b><br/>')
-        result=r.findall(f)
-	#print type(result)
-	for i in result:
-        	print "%s\t %s\t %s\t" %(domain,i[0],i[1])
+    url = urllib.urlopen('http://wap.ip138.com/ip.asp?ip=%s' % domain)
+    f = url.read()
+    r = re.compile(r'&gt; (.*?)<br/><b>查询结果：(.*?)</b><br/>', re.S)
+    result = r.findall(f)
+    # print type(result)
+    for i in result:
+        print u"%s\t %s\t %s\t" % (domain, i[0], i[1].decode('utf-8'))
+
 
 if __name__ == "__main__":
-	if len(sys.argv) < 2:
-		print "请输入IP地址或者域名 (例如:192.168.1.1 / www.baidu.com)"
-		sys.exit()
-	INPUT=sys.argv[1]
-	if not re.findall('(\d{1,3}\.){3}\d{1,3}',INPUT):
-	        if re.findall(r'(\w+\.)?(\w+)(\.\D+){1,2}',INPUT) :
-	                DOMAIN=INPUT
-			DO(DOMAIN)
-	        else:
-	                print "输入的IP地址和域名格式不对！"
-	else:
-	        if ISIP(INPUT)  :
-	                IPADDRESS=INPUT
-			URL(IPADDRESS)
-	        else:
-	                print "IP 地址不合法，请重新输入！"
-
+    if len(sys.argv) < 2:
+        print "请输入IP地址或者域名 (例如:192.168.1.1 / www.baidu.com)"
+        sys.exit()
+    INPUT = sys.argv[1]
+    if not re.findall('(\d{1,3}\.){3}\d{1,3}', INPUT):
+        if re.findall(r'(\w+\.)?(\w+)(\.\D+){1,2}', INPUT):
+            DOMAIN = INPUT
+            DO(DOMAIN)
+        else:
+            print u"输入的IP地址和域名格式不对！"
+    else:
+        if ISIP(INPUT):
+            IPADDRESS = INPUT
+            URL(IPADDRESS)
+        else:
+            print u"IP 地址不合法，请重新输入！"
